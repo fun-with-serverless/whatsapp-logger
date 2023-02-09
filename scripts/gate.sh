05070648a4c67793241fadc8ad1c1f01d4fbb0d3
Ubuntu <ubuntu@ip-172-31-21-56.ec2.internal>
2023-02-09 17:31:28 +0000

    wip

--- scripts/gate.sh
+++ scripts/gate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 # The following bash script is used to perform linting and formatting checks for a collection of projects.
 # and it can be run to validate the code before committing it to the repository.
 
