310d9b3cccb269f357ee58b7c722fd65eba3f38a
Ubuntu <ubuntu@ip-172-31-21-56.ec2.internal>
2023-02-13 20:44:44 +0000

    Improve UI

--- admin-panel/src/utils/consts.py
+++ admin-panel/src/utils/consts.py
@@ -1,91 +1,48 @@
 HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
-<head>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.3.2/spin.min.js"></script>
-  
-  <title>Basic Authentication</title>
-  <style>
-    /* Add some general styles for the form */
-    form {
-      width: 500px;
-      margin: 0 auto;
-      padding: 10px;
-      border: 1px solid gray;
-      border-radius: 5px;
-    }
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/spin.js/2.3.2/spin.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Basic Authentication</title>
+  </head>
+  <body>
+    <div class="container">
+      <div id="login-form" class="form-group">
+        <form>
+          <div class="form-group">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" class="form-control">
+          </div>
+          <input type="button" value="Submit" class="btn btn-primary" onclick="submitForm()">
+        </form>
+      </div>
 
-    /* Add some styles for the form labels */
-    label {
-      font-size: 14px;
-      font-weight: bold;
-      margin-bottom: 5px;
-    }
-
-    /* Add some styles for the form inputs */
-    input[type="text"], input[type="password"], textarea {
-      width: 100%;
-      padding: 12px 20px;
-      margin: 8px 0;
-      box-sizing: border-box;
-      border: 1px solid gray;
-      border-radius: 4px;
-    }
-
-    /* Add some styles for the submit button */
-    input[type="button"] {
-      width: 100%;
-      background-color: #4CAF50;
-      color: white;
-      padding: 14px 20px;
-      margin: 8px 0;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-    /* Add some styles for the image container*/
-    #image-container {
-        padding: 10px;
-        text-align: center;
-    }
-    /* Add some styles for the image*/
-    #image-container img{
-        width:33%;
-    }
-  </style>
-</head>
-<body>
-
-<div id="login-form">
-  <form>
-    <label for="username">Username:</label>
-    <input type="text" id="username" name="username"><br><br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"><br><br>
-    <input type="button" value="Submit" onclick="submitForm()">
-  </form>
-</div>
-
-<div id="config-form" style="display: none;flex-direction: column">
-  <form>
-    <div style="display: flex; align-items: center; margin-bottom: 10px;">
-    <label for="google_secret" style="width: 20%;">Google Secret:</label>
-    <textarea id="google_secret" name="google_secret" style="width: 80%;"></textarea>
-  </div>
-  <div style="display: flex; align-items: center; margin-bottom: 10px;">
-    <label for="sheet_url" style="width: 20%;">Sheet URL:</label>
-    <input type="text" id="sheet_url" name="sheet_url" style="width: 80%;">
-  </div>
-  <div style="display: flex; flex-direction: column; margin-top: 10px;">
-    <input type="button" value="Save" onclick="saveConfig()">
-    <input type="button" value="Show QR" onclick="showQR()">
-  </div>
-  </form>
-  
-</div>
-<div id="spinner-container" style="display: none;"></div>
-<div id="image-container"></div>
-
+      <div id="config-form" style="display: none;">
+        <form>
+          <div class="form-group">
+            <label for="google_secret">Google Secret:</label>
+            <textarea id="google_secret" name="google_secret" class="form-control" style="height: 10em"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="sheet_url">Sheet URL:</label>
+            <input type="text" id="sheet_url" name="sheet_url" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="button" value="Save" class="btn btn-success" onclick="saveConfig()">
+            <input type="button" value="Show QR" class="btn btn-info" onclick="showQR()">
+          </div>
+        </form>
+      </div>
+      <div id="spinner-container" style="display: none;"></div>
+      <div id="image-container" class="text-center"></div>
+    </div>
 <script>
   var spinner = new Spinner({color: '#3d3d3d'}).spin(document.getElementById("spinner-container"));
   
@@ -116,7 +73,7 @@ HTML_TEMPLATE = """
             if (xhr.status === 200) {
                 var response = xhr.response;
                 document.getElementById("login-form").style.display = 'none';
-                document.getElementById("config-form").style.display = 'flex';
+                document.getElementById("config-form").style.display = 'block';
                 document.getElementById("google_secret").innerHTML = response.google_secret;
                 document.getElementById("sheet_url").value = response.sheet_url;
             } else {
