1613b6a2dd1fa05268cf78eaa60f481e33d20d85
Serverless development in Hebrew <support@sharatless.cloud>
2023-04-22 12:58:38 +0300

    Improve chat-gpt (#47)
    Co-authored-by: Efi MK <efi.merdler@gmail.com>

--- backend/src/chatgpt_integration/functions/chatgpt_call/app.py
+++ backend/src/chatgpt_integration/functions/chatgpt_call/app.py
@@ -3,9 +3,10 @@ from dataclasses import asdict
 import functools
 import json
 import os
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 import boto3
 import openai
+import tiktoken
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities import parameters
@@ -40,21 +41,23 @@ def handler(event, context: LambdaContext):
         language = WhatsAppGroup.get(summary["group_id"]).summary_language.value
     except WhatsAppGroup.DoesNotExist:
         logger.info("Missing group id. Using default.", group_id=summary["group_id"])
+    content = _get_valid_content(summary["chats"], language)
     response = chatgpt.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": "You are a helpful assistant.",
+                "content": "You are a stenographer",
             },
             {
                 "role": "user",
-                "content": f"The text is a conversation in a forum where people are discussing various topics. Summerize the following text {summary['chats'][:3930]} Divide the summary into the different topics.Write your answers in {language}",
+                "content": content,
             },
         ],
     )
     logger.info(
         "ChatGPT summary",
+        query_sent=content,
         gpt_reponse=response["choices"][0]["message"]["content"],
         gorup_name=summary["group_name"],
     )
@@ -75,3 +78,50 @@ def _get_s3() -> S3ServiceResource:
 @functools.cache
 def _get_openai():
     return openai
+
+
+@functools.cache
+def _get_encoding():
+    return tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+
+def _get_valid_content(content: str, language: str):
+    string_template = "The text is a conversation in a forum where people are discussing various topics. Summerize the following text by dividing into topics: {}. Write your answers in {}"
+    encoding = _get_encoding()
+    found_string = _find_max_substring(
+        content=content, token_function=lambda val: len(encoding.encode(val))
+    )
+    return string_template.format(found_string, language)
+
+
+def _find_max_substring(
+    content: str, token_function: Callable[[str], int], max_tokens: int = 4000
+):
+    """
+
+    This function finds the longest substring that starts from the beginning of the input string content and
+    satisfies the constraint that the number of tokens (determined by token_function) is not greater than max_tokens.
+
+    Args:
+    content (str): The input string from which the longest substring is to be found.
+    token_function (callable): A function that takes a string as input and returns the number of tokens in the string.
+    max_tokens (int, optional): The maximum number of tokens allowed in the substring. Default is 4000.
+
+    Returns:
+    str: The longest substring that starts from the beginning of the input string and has no more than max_tokens tokens.
+    """
+    left, right = 0, len(content)
+
+    while left < right:
+        mid = (left + right) // 2
+        substring = content[0 : mid + 1]
+        magic_value = token_function(substring)
+
+        if magic_value <= max_tokens:
+            left = mid + 1
+        else:
+            right = mid
+
+    # Here is the maximal index value for the substring that starts with the first element and meets the requirement
+    max_index = left - 1
+    return content[0 : max_index + 1]
--- backend/tests/chatgpt_call/test_app.py
+++ backend/tests/chatgpt_call/test_app.py
@@ -1,6 +1,9 @@
 from unittest.mock import MagicMock, patch
-
-from backend.src.chatgpt_integration.functions.chatgpt_call.app import handler
+import pytest
+from backend.src.chatgpt_integration.functions.chatgpt_call.app import (
+    _find_max_substring,
+    handler,
+)
 from backend.tests.utils import MockOpenAi
 
 
@@ -14,3 +17,25 @@ def test_successful_call_to_chatgpt(get_open_ai, s3_chats, group_db, secret_mana
         "group_id": "group-id",
         "group_name": "gorup-name",
     }
+
+
+@pytest.mark.parametrize(
+    "my_string, max_magic_value, expected",
+    [
+        # Test when the entire string meets the requirement
+        ("abcdef", 1000, "abcdef"),
+        # Test when only the first character meets the requirement
+        ("abcdef", 10, "a"),
+        # Test when no characters meet the requirement
+        ("abcdef", 0, ""),
+        # Test when some characters meet the requirement
+        ("abcdef", 35, "abc"),  # Assuming the magic value of "abc" is less than 4000
+        # Test when the string is empty
+        ("", 1000, ""),
+    ],
+)
+def test_find_max_substring(my_string, max_magic_value, expected):
+    def magic_function(string):
+        return len(string) * 10
+
+    assert _find_max_substring(my_string, magic_function, max_magic_value) == expected
